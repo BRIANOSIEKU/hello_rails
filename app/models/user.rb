7029e40dcc5371c4101ed1a284862e568a99677d
BRIANOSIEKU <brianosieku@gmail.com>
2025-10-02 12:55:42 +0300

    feat(users): add UsersController, routes and JSON endpoints

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+  has_many :facts, dependent: :destroy
+  has_many :user_fact_likes, dependent: :destroy
+
+  validates :username, presence: true, uniqueness: true
+  validates :token, presence: true, uniqueness: true
+
+  before_validation :ensure_token
+
+  private
+
+  def ensure_token
+    self.token ||= SecureRandom.hex(16) # generates a random token
+  end
+end
+
